fbe21ba1aff243a0c63d1b938c372abdb47cb64d
gureumwoon <vfrvfr3221@gmail.com>
2024-10-08 19:27:55 +0900

    리뷰 추가 기능 구현

--- section06/src/app/book/[id]/page.tsx
+++ section06/src/app/book/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
+import { createReviewAction } from "@/actions/create-review.action";
 
 // export const dynamicParams = false;
 export function generateStaticParams() {
@@ -44,21 +45,12 @@ async function BookDetail({ bookId }: { bookId: string }) {
   );
 }
 
-function ReviewEditor() {
-
-  async function createReviewAction(formData: FormData) {
-    "use server";
-
-    const content = formData.get('content')?.toString();
-    const author = formData.get('author')?.toString();
-
-    console.log(content, author);
-  }
-
+function ReviewEditor({ bookId }: { bookId: string }) {
   return <section>
     <form action={createReviewAction}>
-      <input name="content" placeholder="리뷰 내용" />
-      <input name="author" placeholder="작성자" />
+      <input name="bookId" value={bookId} hidden />
+      <input required name="content" placeholder="리뷰 내용" />
+      <input required name="author" placeholder="작성자" />
       <button type="submit">작성하기</button>
     </form>
   </section>
@@ -71,6 +63,6 @@ export default function Page({
 }) {
   return <div className={style.container}>
     <BookDetail bookId={params.id} />
-    <ReviewEditor />
+    <ReviewEditor bookId={params.id} />
   </div>
 }
